323b92af75380f6a061f078d9fe30d5916c11e29
NightTripperId <ndering@wgu.edu>
2021-03-09 11:15:33 -0700

    fixed comments

--- src/com/github/nighttripperid/Demo.java
+++ src/com/github/nighttripperid/Demo.java
@@ -1,4 +1,4 @@
-// A java port of OLC's arbitrary rectangle collision complete solution. Props to javid9x for his excellent tutorial!
+// A java port of OLC's arbitrary rectangle collision complete solution. Props to javidx9 for his excellent tutorial!
 // watch https://youtu.be/8JJ-4JgR7Dg for reference
 package com.github.nighttripperid;
 
